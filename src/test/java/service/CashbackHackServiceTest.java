bc333dab963a8cd4f3807de2851fa0e5b9a60cde
Artemiy Mazur <artemiymazur@yandex.ru>
2025-02-16 11:45:53 +0300

    TestNG commit

--- src/test/java/service/CashbackHackServiceTest.java
+++ src/test/java/service/CashbackHackServiceTest.java
@@ -1,4 +1,36 @@
 package service;
 
+import org.testng.annotations.Test;
+import ru.netology.service.CashbackHackService;
+
 public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void testMoreBoundary() {
+
+        int actual = service.remain(1100);
+        int expected = 900;
+
+        assert (actual == expected);
+    }
+
+    @Test
+    public void testLessBoundary() {
+
+        int actual = service.remain(900);
+        int expected = 100;
+
+        assert (actual == expected);
+    }
+
+    @Test
+    public void testEquallyBoundary() {
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assert (actual == expected);
+    }
 }
